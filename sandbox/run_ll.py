aebf12cba12c4ffe0155f647fde8e5a3f752a030
trillianx <trillianx@gmail.com>
2020-12-10 11:25:50 -0800

    Added more practice methods

--- sandbox/run_ll.py
+++ sandbox/run_ll.py
@@ -3,9 +3,11 @@ from linked_list_practice import Node, LinkedList
 
 if __name__ == "__main__":
     ll = LinkedList()
-    ll.insert_at_head(25)
-    ll.insert_at_head(35)
-    ll.insert_at_tail(100)
-    print(ll.head.data)
-    print(ll.head.next.data)
-    print(ll.head.next.next.data)
+    data = [55, 45, 25, 15, 5]
+    for d in data:
+        ll.insert_at_head(d)
+    ll.insert_at_tail(65)
+    print(ll.get_length())
+    print("")
+    ll.insert_at_index(3, 35)
+    ll.print_ll()
