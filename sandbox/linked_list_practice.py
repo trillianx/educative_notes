d5d81af5d33a9d03132894be43dc9d65ad3995e7
trillianx <trillianx@gmail.com>
2020-12-10 09:55:47 -0800

    Added more linked list practice

--- sandbox/linked_list_practice.py
+++ sandbox/linked_list_practice.py
@@ -1,1 +1,13 @@
-if 
+class Node():
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+    
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def insert_at_head(self, data):
+        new_node = Node(data)
+        self.head = new_node
+    
